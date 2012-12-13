d5bbf6383b4892ba2ce3636f24a33102e7810d4a
Erik Plestenjak <erik.plestenjak@gmail.com>
2012-12-13 15:11:52 +0100

    Set up of "scan it!" button (only framework)

--- src/com/infinitewisdom/scanbar/Main.java
+++ src/com/infinitewisdom/scanbar/Main.java
@@ -1,19 +1,26 @@
 package com.infinitewisdom.scanbar;
 
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import com.google.android.maps.MapActivity;
 
-public class Main extends MapActivity {
+
+public class Main extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        final Button button = (Button) findViewById(R.id.scan_it);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	
+            }
+        });
     }
 
-	@Override
-	protected boolean isRouteDisplayed() {
-		return false;
-	}
+
 }
