a79d4236b7636670d53ec489273007d6362ec205
Dejan <angelovdejan92@gmail.com>
2012-12-13 16:22:16 +0100

    new class for product location objects

--- src/com/infinitewisdom/scanbar/ProductLocation.java
+++ src/com/infinitewisdom/scanbar/ProductLocation.java
@@ -1,5 +1,29 @@
 package com.infinitewisdom.scanbar;
 
 public class ProductLocation {
+	
+	private String latitude;
+	private String longitude;
+	
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	
+	public ProductLocation(String latitude, String longitude) {
+		super();
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	
+	
 
 }
